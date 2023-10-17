8010c4e728c378bf05f7bd2824d6da964de16666
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-10-17 13:48:20 -0700

    Displaying car info in modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -48,7 +48,8 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                             >
 
                                 <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] 
-                                overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                                overflow-y-auto transform rounded-2xl bg-white p-6
+                                text-left shadow-xl transition-all flex flex-col gap-5">
                                     <button type='button' onClick={closeModal} className='absolute rounded-full top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100'>
                                         <Image
                                             src="/close.svg"
@@ -96,6 +97,22 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                         </div>
                                     </div>
 
+                                    <div className='flex-1 flex flex-col gap-1'>
+                                        <h2 className=' font-semibold text-xl capitalize'>
+                                            {car.make} {car.model}
+                                        </h2>
+
+                                        <div className=' mt-3 flex flex-wrap gap-4'>
+
+                                            {Object.entries(car).map(([key, value]) => (
+                                                <div className='flex justify-between gap-5 w-full text-right' key={key}>
+                                                    <h4 className=' text-grey font-bold capitalize'>{key.replace("_", " ")}</h4>
+                                                    <p className=' text-grey font-bold capitalize'>{value}</p>
+                                                </div>
+                                            ))}
+
+                                        </div>
+                                    </div>
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
--- utilities/index.ts
+++ utilities/index.ts
@@ -4,7 +4,7 @@ export async function getCars() {
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=ferrari', {headers: headers
 } );
 
     const results = await response.json();
