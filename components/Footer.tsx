949b9bd3b20d4a08c456797a246831be246403b4
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-10-19 22:03:13 -0700

    Changing logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { footerLinks } from '@/constants'
-import { link } from 'fs'
+
 
 const Footer = () => {
   return (
     <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
         <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
             <div className='flex flex-col justify-start items-start gap-6'>
-                <Image src="/logo.svg" alt="logo"
+                <Image src="/car-logo1.png" alt="logo"
                 width={118}
                 height={18} className='object-contain'/>
                 <p className='text-base text-gray-700'>
-                    RentoRide 2023 <br />
+                    RentaRide 2023 <br />
                     All rights reserved &copy;
                 </p>
             </div>
@@ -38,7 +38,7 @@ const Footer = () => {
             </div>
             <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
 
-                <p>@2023 RentoRide. All Rights Reserved</p>
+                <p>@2023 RentaRide. All Rights Reserved</p>
                 <div className='footer__copyrights-link'>
                     <Link href="/"
                     className='text-gray-500'>
